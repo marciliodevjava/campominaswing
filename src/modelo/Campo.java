29f4a8ef8f71587fd91eb18241642abe8ef0f251
m-arc <m-arc@192.168.1.8>
2022-05-06 17:34:20 -0300

    Class Campo

---
+++ src/modelo/Campo.java
@@ -0,0 +1,117 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+	private final int linha;
+	private final int coluna;
+
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	boolean adicionarVisinho(Campo vizinho) {
+		boolean linhaDiferente = this.linha != vizinho.linha;
+		boolean colunhaDiferente = this.coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunhaDiferente;
+
+		int deltaLinha = Math.abs(this.linha - vizinho.linha);
+		int deltaColunha = Math.abs(this.coluna - vizinho.coluna);
+		int deltaGeral = deltaColunha + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void alternarMarcacao() {
+		if (!this.aberto) {
+			this.marcado = !marcado;
+		}
+	}
+
+	boolean abrir() {
+		if (!this.aberto && !this.marcado) {
+			this.aberto = true;
+
+			if (this.minado) {
+				// TODO Implementar nova versão
+				// FIXME Erro
+			}
+			if (viziancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	boolean viziancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+
+	void minar() {
+		this.minado = true;
+	}
+
+	public boolean isMinado() {
+		return this.minado;
+	}
+
+	public boolean isMarcado() {
+		return this.marcado;
+	}
+
+	void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+
+	public boolean isAberto() {
+		return this.aberto;
+	}
+
+	public boolean isFechado() {
+		return !isAberto();
+	}
+
+	public int getLinha() {
+		return this.linha;
+	}
+
+	public int getColuna() {
+		return this.coluna;
+	}
+
+	boolean objetivoAlcancado() {
+		boolean desvendado = !this.minado && this.aberto;
+		boolean protegido = this.minado && this.marcado;
+		return desvendado || protegido;
+	}
+
+	long minasNaVizianca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+	}
+
+	void reiniciar() {
+		this.aberto = false;
+		this.minado = false;
+		this.marcado = false;
+	}
+
+}
