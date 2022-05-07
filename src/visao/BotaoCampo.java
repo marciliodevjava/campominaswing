7949a5079d3a4c470056bb8c328fa649c0696533
m-arc <m-arc@192.168.1.8>
2022-05-07 19:15:15 -0300

    Class BotaoCampo

---
+++ src/visao/BotaoCampo.java
@@ -0,0 +1,12 @@
+package visao;
+
+import javax.swing.JButton;
+import modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton{
+
+	public BotaoCampo(Campo campo) {
+		
+	}
+}
