77e39d9f596b7146c84572baa2c415d3b84b651c
m-arc <m-arc@192.168.1.8>
2022-05-07 12:06:09 -0300

    Class Temp

---
+++ src/visao/Temp.java
@@ -0,0 +1,12 @@
+package visao;
+
+import modelo.Tabuleiro;
+
+public class Temp {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(3, 3, 9);
+		tabuleiro.abrir(2, 2);
+	}
+}
