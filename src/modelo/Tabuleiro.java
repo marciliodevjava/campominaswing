c0d178ac6a52f0689cfa292bda6d2bab3d96c4e8
m-arc <m-arc@192.168.1.8>
2022-05-07 19:07:09 -0300

    Class Tabuleiro

--- src/modelo/Tabuleiro.java
+++ src/modelo/Tabuleiro.java
@@ -7,9 +7,9 @@ import java.util.function.Predicate;
 
 public class Tabuleiro implements CampoObservador {
 
-	private int linhas;
-	private int colunas;
-	private int minas;
+	private final int linhas;
+	private final int colunas;
+	private final int minas;
 
 	private final List<Campo> campos = new ArrayList<>();
 	private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
@@ -80,6 +80,14 @@ public class Tabuleiro implements CampoObservador {
 	public void reiniciar() {
 		this.campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
+	}
+	
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
 	}
 
 	@Override
